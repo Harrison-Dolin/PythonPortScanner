40261e86c41a4f6dcd6699317faf857f09808462
Harrison-Dolin <57839063+Harrison-Dolin@users.noreply.github.com>
2020-01-29 16:40:06 -0600

    Scans open ports

--- portscanmain.py
+++ portscanmain.py
@@ -4,36 +4,31 @@ import sys
 import socket
 from datetime import datetime
 
-#Define the target for the scan
+# Target determined for the scan
 if len(sys.argv) == 2:
-    #Converting host name to IPv4
-    target = socket.gethostbyname(sys.argv[1])
+    opnport = socket.gethostbyname(sys.argv[1])
 else:
-    print("Invalid arguemnt count.")
+    print("Invalid command.")
     print("Syntax: python3 portscanmain.py <ip address>")
+    
+    
 
-#Include a banner
-print("-" * 50)
 print("Scanning target" + target)
 print("Time: " + str(datetime.now()))
-print("-" * 50)
+
 
 print("\nEnter the range for the scan: ")
 lowBound = input("\nEnter lower bound: ")
 upperBound = input("\nEnter upper bound: ")
 
 try:
-    for port in range(lowBound, upperBound):
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket.setdefaulttimeout(1)
-            result = s.connect_ex((target,port))#Returns error indicator
-            if result == 0:
-                print("Port {} is open".format(port))
+    for prt in range(lowBound, upperBound):
+            soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(0.1)
+            errorHandle = s.connect_ex((opnport,prt))#Returns error indicator
+            if errorHandle == 0:
+                print("Port {} is open".format(prt))
             s.close()
-
-except KeyboardInterrupt:
-    print("\nExit program")
-    sys.exit()
 
 except socket.gaierror:
     print("Hostname couldn't be resolved")
@@ -41,4 +36,7 @@ except socket.gaierror:
 
 except socket.error:
     print("Can't connect to server")
+    sys.exit()
+    
+except KeyboardInterrupt:
     sys.exit()
