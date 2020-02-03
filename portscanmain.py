65956d0e852f4cf71bb33c2f0a6f48578c3c2d06
Harrison-Dolin <57839063+Harrison-Dolin@users.noreply.github.com>
2020-02-02 21:09:30 -0600

    Portscan main

--- portscanmain.py
+++ portscanmain.py
@@ -1,45 +1,52 @@
 #!/bin/python3
 
+from datetime import datetime
 import sys
 import socket
-from datetime import datetime
 
 # Target determined for the scan
 if len(sys.argv) == 2:
-    opnport = socket.gethostbyname(sys.argv[1])
+    opnport = socket.gethostbyname(sys.argv[1]) #Hostname
 else:
     print("Invalid command.")
-    print("Syntax: python3 portscanmain.py <ip address>")
+    print("Syntax: python3 portscanmain.py <ip address>") #Correct syntax for command given to user
     sys.exit()
     
     
-
-print("Scanning target" + opnport)
+#Startup print statement
+print("Scanning target" + opnport) 
 print("Time: " + str(datetime.now()))
 
+#Gets range of ports from user
 print("\nEnter the range for the scan: ")
 lowBound = input("\nEnter lower bound: ")
 upperBound = input("\nEnter upper bound: ")
 
+#Valid range check
 if lowBound > upperBound or lowBound < 0 or upperBound < 0:
     print("Invalid range")
+    sys.exit()
 else:
-    
+    #Loop to scan all ports within range
     try:
      for prt in range(lowBound, upperBound):
         sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
         errorHandle = sc.connect_ex((opnport,prt))#Returns error indicator
-        if errorHandle == 0:
+        if errorHandle == 0: #Check and print for open port
             print("Port {} is open".format(prt))
         sc.close()
 
+    except socket.timeout:
+        print("Error: Program time out")
+        sys.exit()
+        
     except socket.gaierror:
-        print("Hostname couldn't be resolved")
+        print("Error: Hostname couldn't be resolved")
         sys.exit()
 
     except socket.error:
-        print("Can't connect to server")
+        print("Error: Can't connect to server")
         sys.exit()
     
     except KeyboardInterrupt:
